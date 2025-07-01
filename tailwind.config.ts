db6cd4b6bb92b0d54069d396810fa33d149f4a73
Ashboy64 <ashish.arartc@gmail.com>
2025-07-01 07:47:20 +0200

    Add image to chekhov stories post, more progress on rust lambda calculus post

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ module.exports = {
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './posts/**/*.mdx', // Make sure your MDX files are included here
     ],
     theme: {
         extend: {
